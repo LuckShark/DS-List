eb46bc89466635b91d30668800d267e9fb7675fd
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-12 15:22:10 -0300

    Game seed in db

--- src/main/java/com/luckshark/dslist/entities/Game.java
+++ src/main/java/com/luckshark/dslist/entities/Game.java
@@ -15,21 +15,25 @@ public class Game {
     @Column(name = "game_year") //customizei o nome da coluna
     private Integer year; //o year é uma palavra reservada do SQL, por isso fiz a notação
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    //Tive que ajeitar essas duas para passar de 255 caracteres
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -70,12 +74,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
