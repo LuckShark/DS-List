91938cb0eeff90a5bddb5865ab62298c8355bede
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-12 15:43:20 -0300

    DTO created

---
+++ src/main/java/com/luckshark/dslist/dto/GameMinDTO.java
@@ -0,0 +1,47 @@
+package com.luckshark.dslist.dto;
+
+
+import com.luckshark.dslist.entities.Game;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Game entity) { //fazendo isso, eu copio o que está no game
+        //na verdade, nem precisa do THIS, por que não tem ambiguidade
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+    //No DTO, só precia de GETTERs
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+}
