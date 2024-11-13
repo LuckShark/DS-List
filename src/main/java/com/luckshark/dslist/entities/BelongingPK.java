dd3c42426e0eb15fff595de3460a9d95a9b717d1
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-13 15:38:09 -0300

    modelo de dominio implementado

---
+++ src/main/java/com/luckshark/dslist/entities/BelongingPK.java
@@ -0,0 +1,64 @@
+package com.luckshark.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable //Por que tenho um tipo representando dois campos na table
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList list;
+
+    public BelongingPK() {
+    }
+
+    public BelongingPK(Game game, GameList list) {
+        this.game = game;
+        this.list = list;
+    }
+
+    //Getters e setters
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getList() {
+        return list;
+    }
+
+    public void setList(GameList list) {
+        this.list = list;
+    }
+
+    //comparar uma chave primária multipla
+    //então tenho que comprar os dois
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        BelongingPK other = (BelongingPK) obj;
+        return game.equals(other.game) && list.equals(other.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, list);
+    }
+}
