718da4ecc3e11a3f525e79b93a873144179f45bf
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-13 08:14:03 -0300

    gameservice created

---
+++ src/main/java/com/luckshark/dslist/services/GameService.java
@@ -0,0 +1,24 @@
+package com.luckshark.dslist.services;
+
+import com.luckshark.dslist.dto.GameMinDTO;
+import com.luckshark.dslist.entities.Game;
+import com.luckshark.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+//Eu preciso registrar esse componente, diferente do Repository (poderia usar @Component tbm)
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    //Respeita a arquitetura: esse service devolve um DTO
+    public List<Game> findAll() {
+        List<Game> result = gameRepository.findAll(); //gera uma consulta no banco que busca todos os games, e é convertida em uma lista de games que cai numa variável (result)
+        return result;
+    }
+}
